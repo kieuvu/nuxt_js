725e5ec2575699a997494e2c960126c4777254bf
“kieuvu” <kieuvuvuvu@gmail.com>
2022-11-01 09:30:25 +0700

    vuex store

---
+++ store/index.js
@@ -0,0 +1,52 @@
+import { Store } from 'vuex';
+// import authServices from '~/services/auth.service';
+// import tokenService from '~/services/token.service';
+
+const createStore = () => {
+  return new Store({
+    state: {
+    },
+
+    mutations: {
+    },
+
+    actions: {
+      nuxtServerInit(context, payload) {
+      },
+
+      login: async function (context, payload) {
+        const response = await this.$auth.loginWith('local', {
+          data: payload
+        });
+
+        if (response.data?.error) {
+          return {
+            status: false,
+            data: response.data.details
+          };
+        } else {
+          return {
+            status: true,
+          };
+        }
+      },
+    },
+
+    getters: {
+      isAuthenticated(state) {
+        return state.auth.loggedIn;
+      },
+
+      loggedInUser(state) {
+        return state.auth.user;
+      }
+    },
+  });
+};
+
+export default createStore;
+
+
+
+
+
