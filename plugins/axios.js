0513cd929f0d37a77204952d6b36cbd799e14ee2
“kieuvu” <kieuvuvuvu@gmail.com>
2022-11-02 16:05:43 +0700

    config axios

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,8 +1,19 @@
-export default function ({ $axios, redirect }) {
+export default function (context) {
+
+  const $axios = context.$axios;
+
   $axios.defaults.headers.common['SBFW-Api-Token'] = 'SBFWANLAB2022';
   $axios.defaults.baseURL = "http://sbkintone.ga/api";
 
   $axios.onRequest(config => {
+    const accessToken = context.$auth.strategy.token.get();
+    const refreshToken = context.$auth.strategy.refreshToken.get();
+
+    if (accessToken) {
+      $axios.defaults.headers.common.Authorization = accessToken;
+      $axios.defaults.headers.common.Refreshtoken = refreshToken;
+    }
+
     return config;
   });
 
